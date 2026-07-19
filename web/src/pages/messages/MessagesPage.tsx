eb7af9feda9c571e140443a9f8892752b6e7fb47
lenny <cocoblackett@gmail.com>
2026-07-19 13:25:27 +0200

    Update MessagesPage.tsx

--- web/src/pages/messages/MessagesPage.tsx
+++ web/src/pages/messages/MessagesPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import api from '../../api/axios';
 import { useUser } from '../../contexts/useUser';
